626d670e6415e5435f059cde6ca7dde57399651c
shaggy611 <sevczik11@gmail.com>
2023-10-23 04:03:38 +0300

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                'privacy-policy': resolve(__dirname, 'public/privacy-policy/index.html'),
-                'public-offer': resolve(__dirname, 'public/public-offer/index.html')
+                'privacy-policy': resolve(__dirname, 'privacy-policy.html'),
+                'public-offer': resolve(__dirname, 'public-offer.html')
                 // pages_2: resolve(__dirname, 'pages/chief_collection_manager_1.html'),
             },
             external: [
