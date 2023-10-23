1977b9d8a58dbfc64adc5aeb10c342b668042a47
shaggy611 <sevczik11@gmail.com>
2023-10-23 03:31:05 +0300

    add animation

--- main.js
+++ main.js
@@ -11,6 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const productGallery_1 = document.querySelector('#product-gallery_1')
     const productGallery_2 = document.querySelector('#product-gallery_2')
 
+    // function showBlockWhedView() {
+    //     let scrollPosition = window.screenY
+    //     let targetPosition = fiveStepsSection.offsetTop
+
+    //     if (scrollPosition >= 100) {
+    //         console.log('Користувач прокрутив до цільового елемента');
+    //         fiveStepsSection.classList.add('activate-animation')
+    //     }
+    // }
+
     if (productGallery_1) {
         const gallery_1 = new Gallery('#product-gallery_1').initialize()
     }
@@ -20,3 +30,15 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
+const fiveStepsSection = document.querySelector('#five-steps__scroll-spy-js')
+let scrollPosition
+let targetPosition
+
+window.addEventListener("scroll", function (e) {
+    scrollPosition = window.scrollY
+    targetPosition = fiveStepsSection.offsetTop
+
+    if (scrollPosition >= (targetPosition - 150)) {
+        fiveStepsSection.classList.add('activate-animation')
+    }
+})
