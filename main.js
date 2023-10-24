8d5e841913c9963c3b124e762e5878a73e2c2fc2
shaggy611 <sevczik11@gmail.com>
2023-10-24 16:23:49 +0300

    done menu

--- main.js
+++ main.js
@@ -10,16 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const productGallery_1 = document.querySelector('#product-gallery_1')
     const productGallery_2 = document.querySelector('#product-gallery_2')
-
-    // function showBlockWhedView() {
-    //     let scrollPosition = window.screenY
-    //     let targetPosition = fiveStepsSection.offsetTop
-
-    //     if (scrollPosition >= 100) {
-    //         console.log('Користувач прокрутив до цільового елемента');
-    //         fiveStepsSection.classList.add('activate-animation')
-    //     }
-    // }
+    const menuToggler = document.querySelector('#show-mobile-menu')
+    const mobileMenu = document.querySelector('#mobile-menu-roller-js')
 
     if (productGallery_1) {
         const gallery_1 = new Gallery('#product-gallery_1').initialize()
@@ -28,9 +20,25 @@ document.addEventListener('DOMContentLoaded', () => {
     if (productGallery_2) {
         const gallery_2 = new Gallery('#product-gallery_2').initialize()
     }
+
+    menuToggler.addEventListener('click', function () {
+        if (menuToggler.checked) {
+            document.body.style.setProperty('overflow', 'hidden')
+        } else {
+            document.body.style.setProperty('overflow-y', 'scroll')
+        }
+    })
+
+    mobileMenu.addEventListener('click', function (event) {
+        if (event.target.classList.contains('mobile-menu__item_link')) {
+            document.body.style.setProperty('overflow-y', 'scroll')
+            menuToggler.checked = false
+        }
+    })
+
 })
 
-const fiveStepsSection = document.querySelector('#five-steps__scroll-spy-js')
+const fiveStepsSection = document.querySelector('.five-steps__scroll-spy-js')
 let scrollPosition
 let targetPosition
 
