f5e20a45cc0796ab3badb261ca366801fd3f0a07
shaggy611 <sevczik11@gmail.com>
2023-10-26 01:21:13 +0300

    eng version

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
                 main: resolve(__dirname, 'index.html'),
                 'privacy-policy': resolve(__dirname, 'privacy-policy.html'),
                 'public-offer': resolve(__dirname, 'public-offer.html'),
-                'eng': resolve(__dirname, 'public/eng/index.html')
+                'eng': resolve(__dirname, 'eng/index.html')
                 // pages_2: resolve(__dirname, 'pages/chief_collection_manager_1.html'),
             },
             external: [
